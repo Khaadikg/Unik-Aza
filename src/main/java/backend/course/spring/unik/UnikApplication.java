aaab1dcd44ce67ff660379029736a22153db443f
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-03-06 21:54:03 +0500

    first commit: create project

--- src/main/java/backend/course/spring/unik/UnikApplication.java
+++ src/main/java/backend/course/spring/unik/UnikApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnikApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnikApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UnikApplication.class, args);
+    }
 
 }
