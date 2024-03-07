560ef9f665f212ad5b739d8c5313a0fafb3bce20
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-03-07 11:33:39 +0500

    Add new dependencies for project and security with jwt token

---
+++ src/main/java/backend/course/spring/unik/entity/User.java
@@ -0,0 +1,67 @@
+package backend.course.spring.unik.entity;
+
+import backend.course.spring.unik.entity.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String username;
+    String email;
+    String password;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/backend/course/spring/unik/entity/enums/Role.java
@@ -0,0 +1,5 @@
+package backend.course.spring.unik.entity.enums;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_USER
+}
